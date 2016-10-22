788868cfb622685ea80b6e806fffc469e658f454
zootrash <zootrash@users.noreply.github.com>
2016-10-22 15:00:12 -0700

    followed a tutorial from the official android site and a simple ui design working!

--- senproj/app/src/main/java/com/example/jenninb/myapplication/MainActivity.java
+++ senproj/app/src/main/java/com/example/jenninb/myapplication/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.jenninb.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;       // building block for ui components
+import android.content.Intent;  // operations to be performed
+import android.widget.EditText; // text fields
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /* when user clicks the send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }
