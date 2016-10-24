b245e58224b1dc7c6222401dd14f5bfd2aa75902
brett1337 <brett1337@gmail.com>
2016-10-24 10:34:37 -0700

    Added second activity

--- senproj/app/src/main/java/com/example/jenninb/myapplication/MainActivity.java
+++ senproj/app/src/main/java/com/example/jenninb/myapplication/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.jenninb.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
-
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 }
